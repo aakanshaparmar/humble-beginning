f6ac78fba3c5660bf4fc3522993d5f71e15adad6
aakanshaparmar <parmar.aakansha@gmail.com>
2016-02-11 20:58:30 +0800

    Task 3 AJAX request done

---
+++ task3/scripts/emailScript.js
@@ -0,0 +1,10 @@
+function loadDoc() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (xhttp.readyState == 4 && xhttp.status == 200) {
+      document.getElementById("emailDetails").innerHTML = xhttp.responseText;
+    }
+  };
+  xhttp.open("GET", "https://db4.insightglobe.net/career/mail", true);
+  xhttp.send();
+}
