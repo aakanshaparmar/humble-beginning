190d44c85e401cc8af88000f66650712d41126a3
aakanshaparmar <parmar.aakansha@gmail.com>
2016-02-12 00:14:39 +0800

    Task 3 AJAX request done

--- task3/scripts/emailScript.js
+++ task3/scripts/emailScript.js
@@ -1,10 +1,25 @@
 function loadDoc() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (xhttp.readyState == 4 && xhttp.status == 200) {
-      document.getElementById("emailDetails").innerHTML = xhttp.responseText;
+  // var xhttp = new XMLHttpRequest();
+  // xhttp.onreadystatechange = function() {
+  //   if (xhttp.readyState == 4 && xhttp.status == 200) {
+  //     document.getElementById("emailDetails").innerHTML = xhttp.responseText;
+  //   }
+  // };
+  // xhttp.open("GET", "https://db4.insightglobe.net/career/mail", true);
+  // xhttp.send();
+
+   $.ajax({
+
+    url: 'https://db4.insightglobe.net/career/mail',
+	type: "GET",
+    dataType: "jsonp",
+    success: function (msg) {
+         document.getElementById('emailDetails').innerHtml=msg;
+    },
+    error: function () {
+        document.getElementById('emailDetails').innerHtml='Error';
     }
-  };
-  xhttp.open("GET", "https://db4.insightglobe.net/career/mail", true);
-  xhttp.send();
+
+});
+
 }
