98b69ba885ecfd2c9af0b28edbbc3005402a48f9
aakanshaparmar <parmar.aakansha@gmail.com>
2016-02-14 03:51:34 +0800

    Task 3 formatting partly done

--- task3/scripts/emailScript.js
+++ task3/scripts/emailScript.js
@@ -1,25 +1,43 @@
-function loadDoc() {
-  // var xhttp = new XMLHttpRequest();
-  // xhttp.onreadystatechange = function() {
-  //   if (xhttp.readyState == 4 && xhttp.status == 200) {
-  //     document.getElementById("emailDetails").innerHTML = xhttp.responseText;
-  //   }
-  // };
-  // xhttp.open("GET", "https://db4.insightglobe.net/career/mail", true);
-  // xhttp.send();
+function loadInbox(){
+            var data_file = "https://db4.insightglobe.net/career/mail";
+            var http_request = new XMLHttpRequest();
+            try{
+               // Opera 8.0+, Firefox, Chrome, Safari
+               http_request = new XMLHttpRequest();
+            }catch (e){
+               // Internet Explorer Browsers
+               try{
+                  http_request = new ActiveXObject("Msxml2.XMLHTTP");
+          
+               }catch (e) {
+        
+                  try{
+                     http_request = new ActiveXObject("Microsoft.XMLHTTP");
+                  }catch (e){
+                     // Something went wrong
+                     alert("Sorry! Your browser broke!");
+                     return false;
+                  }
+          
+               }
+            }
+      
+            http_request.onreadystatechange = function(){
+      
+               if (http_request.readyState == 4  ){
+                  // Javascript function JSON.parse to parse JSON data
+                  var jsonObj = JSON.parse(http_request.responseText);
 
-   $.ajax({
+                  // jsonObj variable now contains the data structure and can
+                  // be accessed as jsonObj.name and jsonObj.country.
+                  document.getElementById("Name").innerHTML = jsonObj.mails[1].from;
+                  document.getElementById("Country").innerHTML = jsonObj.to;
+               }
+            }
+      
+            http_request.open("GET", data_file, true);
+            http_request.send();
+         }
+    
 
-    url: 'https://db4.insightglobe.net/career/mail',
-	type: "GET",
-    dataType: "jsonp",
-    success: function (msg) {
-         document.getElementById('emailDetails').innerHtml=msg;
-    },
-    error: function () {
-        document.getElementById('emailDetails').innerHtml='Error';
-    }
-
-});
-
-}
+  
